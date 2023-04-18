2481c5691ab84d763a3279965933589934d243fc
smarcosm <sebastiaomarcos49@gmail.com>
2023-04-18 19:01:50 -0300

    feat: :sparkles: Tornando a classe category um AggrefateRoot

--- application/src/main/java/com/smarcosm/admin_catalogo/application/UseCase.java
+++ application/src/main/java/com/smarcosm/admin_catalogo/application/UseCase.java
@@ -3,6 +3,7 @@ package com.smarcosm.admin_catalogo.application;
 import com.smarcosm.admin_catalogo.domain.category.Category;
 
 public class UseCase {
+
     public Category execute(){
         return new Category();
     }
