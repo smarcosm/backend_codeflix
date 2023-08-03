f370e08bd02afb86666147650f8a2b5864607947
smarcosm <sebastiaomarcos49@gmail.com>
2023-08-03 19:26:10 -0300

    chore: :wrench: Adicionando o springboot na camada de infrastructure

---
+++ infrastructure/src/main/java/com/smarcosm/admin_catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,10 @@
+package com.smarcosm.admin_catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.smarcosm.admin_catalogo")
+
+public class WebServerConfig {
+}
