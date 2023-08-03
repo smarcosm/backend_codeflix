c5796cfa3a874e5a5525ec2a430c50a2508a4a6c
smarcosm <sebastiaomarcos49@gmail.com>
2023-08-03 19:44:24 -0300

    chore: :wrench: Configurando os profiles do springboot

--- infrastructure/src/main/java/com/smarcosm/admin_catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/smarcosm/admin_catalogo/infrastructure/Main.java
@@ -3,12 +3,14 @@ package com.smarcosm.admin_catalogo.infrastructure;
 import com.smarcosm.admin_catalogo.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
