60c24f972c504c08621467569d6647d1780a85d3
smarcosm <sebastiaomarcos49@gmail.com>
2023-08-04 19:18:39 -0300

    chore: :wrench: Configurando o datasource de producao da aplicacao

--- infrastructure/src/main/java/com/smarcosm/admin_catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/smarcosm/admin_catalogo/infrastructure/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "production");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
