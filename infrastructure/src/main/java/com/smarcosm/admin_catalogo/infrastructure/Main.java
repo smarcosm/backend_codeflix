57469bc27593399be18834290bcca7631c889aa3
smarcosm <sebastiaomarcos49@gmail.com>
2023-08-04 22:55:01 -0300

    chore: :wrench: Customizando o hibernete e o hikari para alta performance

--- infrastructure/src/main/java/com/smarcosm/admin_catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/smarcosm/admin_catalogo/infrastructure/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "production");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
