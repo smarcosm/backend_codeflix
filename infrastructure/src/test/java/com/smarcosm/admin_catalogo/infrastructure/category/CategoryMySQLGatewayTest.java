90f24d058ae8af89f6d7eee0750472a41309c5b2
smarcosm <sebastiaomarcos49@gmail.com>
2023-08-09 18:30:29 -0300

    test: :rotating_light:Testes integrado do metodo do gateway de categoria

--- infrastructure/src/test/java/com/smarcosm/admin_catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
+++ infrastructure/src/test/java/com/smarcosm/admin_catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -1,25 +1,11 @@
 package com.smarcosm.admin_catalogo.infrastructure.category;
 
 import com.smarcosm.admin_catalogo.domain.category.Category;
-import com.smarcosm.admin_catalogo.infrastructure.MySQLGatewayTest;
+import com.smarcosm.admin_catalogo.infrastructure.category.persitence.CategoryJpaEntity;
 import com.smarcosm.admin_catalogo.infrastructure.category.persitence.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.util.Collection;
-import java.util.Collections;
-@MySQLGatewayTest
 public class CategoryMySQLGatewayTest {
     @Autowired
     private CategoryMySQLGateway categoryGateway;
@@ -58,4 +44,43 @@ public class CategoryMySQLGatewayTest {
         Assertions.assertNull(actualCategory.getDeletedAt());
     }
 
+    @Test
+    public void givenAValidCategory_whenCallsUpdate_shouldReturnCategoryUpdated(){
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final  var expectedIsActive = true;
+
+        final var aCategory = Category.newCategory("Film", null, expectedIsActive);
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        categoryRepository.save(CategoryJpaEntity.from(aCategory));
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        final var aUpdateCategory = aCategory.clone();
+        final var actualCategory = categoryGateway.update(aUpdateCategory);
+
+        Assertions.assertEquals(1, categoryRepository.count());
+
+
+        Assertions.assertEquals(aCategory.getId(), actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
+        Assertions.assertTrue(aCategory.getUpdatedAt().isBefore(actualCategory.getUpdatedAt()));
+        Assertions.assertEquals(aCategory.getDeletedAt(), actualCategory.getDeletedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+
+        final var actualEntity = categoryRepository.findById(aCategory.getId().getValue()).get();
+
+        Assertions.assertEquals(aCategory.getId().getValue(), actualEntity.getId());
+        Assertions.assertEquals(expectedName, actualEntity.getName());
+        Assertions.assertEquals(expectedDescription, actualEntity.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualEntity.isActive());
+        Assertions.assertEquals(aCategory.getCreatedAt(), actualEntity.getCreatedAt());
+        Assertions.assertTrue(aCategory.getUpdatedAt().isBefore(actualCategory.getUpdatedAt()));
+        Assertions.assertEquals(aCategory.getDeletedAt(), actualEntity.getDeletedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
 }
