868c9b0209c1b33a20e8beb889e57c2af78ce4df
smarcosm <sebastiaomarcos49@gmail.com>
2023-11-21 18:06:22 -0300

    test: :rotating_light: Testes e2e da criacao de categoria

--- infrastructure/src/test/java/com/smarcosm/admin_catalogo/e2e/category/CategoryE2ETest.java
+++ infrastructure/src/test/java/com/smarcosm/admin_catalogo/e2e/category/CategoryE2ETest.java
@@ -1,13 +1,20 @@
 package com.smarcosm.admin_catalogo.e2e.category;
 
 import com.smarcosm.admin_catalogo.E2ETest;
+import com.smarcosm.admin_catalogo.domain.category.CategoryID;
+import com.smarcosm.admin_catalogo.infrastructure.category.models.CategoryResponse;
+import com.smarcosm.admin_catalogo.infrastructure.category.models.CreateCategoryRequest;
 import com.smarcosm.admin_catalogo.infrastructure.category.persitence.CategoryRepository;
+import com.smarcosm.admin_catalogo.infrastructure.configuration.json.Json;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -32,5 +39,45 @@ public class CategoryE2ETest {
     @Test
     public void asACatalogoAdminIShouldBeAbleToCreateANewCategoryWithValidValues() throws Exception {
         Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+
+        Assertions.assertEquals(0, categoryRepository.count());
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualId = givenACategory(expectedName, expectedDescription, expectedIsActive);
+        final var actualCategory = retrieveACategory(actualId.getValue());
+
+        Assertions.assertEquals(expectedName, actualCategory.name());
+        Assertions.assertEquals(expectedDescription, actualCategory.description());
+        Assertions.assertEquals(expectedIsActive, actualCategory.active());
+        Assertions.assertNotNull(actualCategory.createdAt());
+        Assertions.assertNotNull(actualCategory.updatedAt());
+        Assertions.assertNull(actualCategory.deletedAt());
+    }
+    private CategoryID givenACategory(final String aName, final String aDescription, final boolean isActive) throws Exception {
+      final var aRequestBody  = new CreateCategoryRequest(aName, aDescription, isActive);
+        final var aRequest = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Json.writeValueAsString(aRequestBody));
+
+        final var actualId = this.mvc.perform(aRequest)
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn()
+                .getResponse().getHeader("Location")
+                .replace("/categories", "");
+
+        return CategoryID.from(actualId);
+    }
+    private CategoryResponse retrieveACategory(final String anId) throws Exception {
+        final var aRequest = MockMvcRequestBuilders.get("/categories/" + anId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        final var json = this.mvc.perform(aRequest)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn()
+                .getResponse().getContentAsString();
+        return Json.readValue(json, CategoryResponse.class);
     }
 }
