097df2c01dc8e73a59f01d5276848ac74b843e77
smarcosm <sebastiaomarcos49@gmail.com>
2023-12-14 10:54:54 -0300

    test: :rotating_light: Testes unitarios da nova entidade de genre

---
+++ domain/src/test/java/com/smarcosm/admin_catalogo/domain/genre/GenreTest.java
@@ -0,0 +1,78 @@
+package com.smarcosm.admin_catalogo.domain.genre;
+
+import com.smarcosm.admin_catalogo.domain.exception.DomainException;
+import com.smarcosm.admin_catalogo.domain.validation.handler.ThrowsValidationHandler;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GenreTest {
+    @Test
+    public void givenValidParams_whenCallNewGenre_shouldInstantiateAGenre(){
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = 0;
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        Assertions.assertNotNull(actualGenre.getId());
+        Assertions.assertEquals(expectedName, actualGenre.getName());
+        Assertions.assertEquals(expectedIsActive, actualGenre.isActive());
+        Assertions.assertEquals(expectedCategories, actualGenre.getCategories().size());
+        Assertions.assertNotNull(actualGenre.getCreatedAt());
+        Assertions.assertNotNull(actualGenre.getUpdatedAt());
+        Assertions.assertNull(actualGenre.getDeletedAt());
+    }
+
+    @Test
+    public void givenInvalidNullName_whenCallNewGenreAndValidate_shouldReceiveError(){
+        final String expectedName = null;
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+       final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+       Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+       Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+    @Test
+    public void givenInvalidEmptyName_whenCallNewGenreAndValidate_shouldReceiveError(){
+        final var expectedName = " ";
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+    @Test
+    public void givenInvalidNameWithLengthGreaterThan255_whenCallNewGenreAndValidate_shouldReceiveError(){
+        final var expectedName = """
+                        O incentivo ao avanço tecnológico, assim como o entendimento das metas propostas não pode mais se dissociar das formas de ação.
+                        Do mesmo modo, a valorização de fatores subjetivos garante a contribuição de um grupo importante na determinação das novas proposições.
+                        O que temos que ter sempre em mente é que o início da atividade geral de formação de atitudes agrega valor ao estabelecimento das condições financeiras e administrativas exigidas.
+                                """;
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 1 and 255 characters";
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+}
--- infrastructure/src/main/java/com/smarcosm/admin_catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/smarcosm/admin_catalogo/infrastructure/Main.java
@@ -1,29 +1,13 @@
 package com.smarcosm.admin_catalogo.infrastructure;
 
-import com.smarcosm.admin_catalogo.application.category.create.CreateCategoryUseCase;
-import com.smarcosm.admin_catalogo.application.category.delete.DeleteCategoryUseCase;
-import com.smarcosm.admin_catalogo.application.category.retrieve.get.GetCategoryByIdUseCase;
-import com.smarcosm.admin_catalogo.application.category.retrieve.list.ListCategoriesUseCase;
-import com.smarcosm.admin_catalogo.application.category.update.UpdateCategoryUseCase;
-import com.smarcosm.admin_catalogo.domain.category.Category;
-import com.smarcosm.admin_catalogo.infrastructure.category.persitence.CategoryJpaEntity;
-import com.smarcosm.admin_catalogo.infrastructure.category.persitence.CategoryRepository;
 import com.smarcosm.admin_catalogo.infrastructure.configuration.WebServerConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
