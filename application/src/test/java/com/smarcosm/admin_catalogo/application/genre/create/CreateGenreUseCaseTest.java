acc3874c881b938bc9048f8b719d02f8e55fb343
smarcosm <sebastiaomarcos49@gmail.com>
2024-03-08 16:57:17 -0300

    test: :rotating_light: Testes unitarios do caso de uso de criacao de genre

---
+++ application/src/test/java/com/smarcosm/admin_catalogo/application/genre/create/CreateGenreUseCaseTest.java
@@ -0,0 +1,59 @@
+package com.smarcosm.admin_catalogo.application.genre.create;
+
+import com.smarcosm.admin_catalogo.domain.category.CategoryGateway;
+import com.smarcosm.admin_catalogo.domain.category.CategoryID;
+import com.smarcosm.admin_catalogo.domain.genre.GenreGateway;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+
+@ExtendWith(MockitoExtension.class)
+public class CreateGenreUseCaseTest {
+    @Mock
+    private DefaltCreateGenreUseCase useCase;
+    @Mock
+    private CategoryGateway categoryGateway;
+    @Mock
+    private GenreGateway genreGateway;
+    @Test
+    public void givenAValidCommand_whenCallsCreateGenre_shouldReturnGenreId(){
+        // given
+        final var expectName= "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.<CategoryID>of();
+
+        final var aCommand = CreateGenreCommand.with(expectName, expectedIsActive, asString(expectedCategories));
+
+        Mockito.when(genreGateway.create(any()))
+                .thenAnswer(returnsFirstArg());
+        //when
+        final var actualOutput = useCase.execute(aCommand);
+
+        //then
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        Mockito.verify(genreGateway, Mockito.times(1)).create(argThat(aGenre ->
+                Objects.equals(expectName, aGenre.getName())
+                && Objects.equals(expectedIsActive, aGenre.isActive())
+                && Objects.equals(expectedCategories, aGenre.getCategories())
+                && Objects.nonNull(aGenre.getId())
+                && Objects.nonNull(aGenre.getCreatedAt())
+                && Objects.nonNull(aGenre.getUpdatedAt())
+                && Objects.isNull(aGenre.getDeletedAt())
+        ));
+    }
+
+    private List<String> asString(final List<CategoryID> categories){
+        return categories.stream().map(CategoryID::getValue).toList();
+    }
+}
