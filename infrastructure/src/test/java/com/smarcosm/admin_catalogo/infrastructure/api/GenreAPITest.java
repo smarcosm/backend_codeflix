aafe033b7902ad44b53bae323b31dc323d2b115e
smarcosm <sebastiaomarcos49@gmail.com>
2024-06-06 13:49:28 -0300

    test: :rotating_light: Testes integrados do endpoint de get de genre por id

--- infrastructure/src/test/java/com/smarcosm/admin_catalogo/infrastructure/api/GenreAPITest.java
+++ infrastructure/src/test/java/com/smarcosm/admin_catalogo/infrastructure/api/GenreAPITest.java
@@ -5,9 +5,14 @@ import com.smarcosm.admin_catalogo.ControllerTest;
 import com.smarcosm.admin_catalogo.application.genre.create.CreateGenreOutput;
 import com.smarcosm.admin_catalogo.application.genre.create.CreateGenreUseCase;
 import com.smarcosm.admin_catalogo.application.genre.delete.DeleteGenreUseCase;
+import com.smarcosm.admin_catalogo.application.genre.retrieve.get.GenreOutput;
 import com.smarcosm.admin_catalogo.application.genre.retrieve.get.GetGenreByIdUseCase;
 import com.smarcosm.admin_catalogo.application.genre.update.UpdateGenreUseCase;
+import com.smarcosm.admin_catalogo.domain.category.CategoryID;
+import com.smarcosm.admin_catalogo.domain.exception.NotFoundException;
 import com.smarcosm.admin_catalogo.domain.exception.NotificationException;
+import com.smarcosm.admin_catalogo.domain.genre.Genre;
+import com.smarcosm.admin_catalogo.domain.genre.GenreID;
 import com.smarcosm.admin_catalogo.domain.validation.Error;
 import com.smarcosm.admin_catalogo.domain.validation.handler.Notification;
 import com.smarcosm.admin_catalogo.infrastructure.genre.models.CreateGenreRequest;
@@ -24,9 +29,9 @@ import java.util.List;
 import java.util.Objects;
 
 import static org.hamcrest.Matchers.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -107,6 +112,66 @@ public class GenreAPITest {
                         && Objects.equals(expectedIsActive, cmd.isActive())
 
         ));
+    }
+    @Test
+    public void givenAValidId_whenCallsGetGenreById_shouldReturnGenre() throws Exception {
+        // given
+        final var expectedName = "Ação";
+        final var expectedCategories = List.of("123", "456");
+        final var expectedIsActive = false;
+
+
+        final var aGenre = Genre.newGenre(expectedName, expectedIsActive)
+                .addCategories(expectedCategories.stream()
+                        .map(CategoryID::from)
+                        .toList()
+                );
+        final var expectedId = aGenre.getId().getValue();
+
+        when(getGenreByIdUseCase.execute(any()))
+                .thenReturn(GenreOutput.from(aGenre));
+
+        // when
+        final var aRequest = get("/genres/{id}", expectedId)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(MediaType.APPLICATION_JSON_VALUE);
+        final var response = this.mvc.perform(aRequest);
+        // then
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", equalTo(expectedId)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", equalTo(expectedName)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.categories_id", equalTo(expectedCategories)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.is_active", equalTo(expectedIsActive)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.created_at", equalTo(aGenre.getCreatedAt().toString())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.updated_at", equalTo(aGenre.getUpdatedAt().toString())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.deleted_at", equalTo(aGenre.getDeletedAt().toString())));
+
+        Mockito.verify(getGenreByIdUseCase).execute(eq(expectedId));
+    }
+    @Test
+    public void givenAnInvalidId_whenCallsGetGenreById_shouldReturnNotFound() throws Exception  {
+        // given
+        final var expectedErrorMessage = "Genre with ID 123 was not found";
+        final var expectedId = GenreID.from("123");
+
+
+
+        when(getGenreByIdUseCase.execute(any()))
+                .thenThrow(NotFoundException.with(Genre.class, expectedId));
+
+        // when
+        final var aRequest = get("/genres/{id}", expectedId)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(MediaType.APPLICATION_JSON_VALUE);
+        final var response = this.mvc.perform(aRequest);
+        // then
+        response.andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", equalTo(expectedErrorMessage)));
+
+
+        Mockito.verify(getGenreByIdUseCase).execute(eq(expectedId.getValue()));
     }
 
 }
