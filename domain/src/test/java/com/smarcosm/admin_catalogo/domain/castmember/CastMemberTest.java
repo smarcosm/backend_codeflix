2a2da957ec5865abef167a3b1f119c4148a7ab07
smarcosm <sebastiaomarcos49@gmail.com>
2024-06-11 19:39:25 -0300

    test: :rotating_light: TDD da nova entidade de castmember

---
+++ domain/src/test/java/com/smarcosm/admin_catalogo/domain/castmember/CastMemberTest.java
@@ -0,0 +1,25 @@
+package com.smarcosm.admin_catalogo.domain.castmember;
+
+import com.smarcosm.admin_catalogo.domain.category.Category;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CastMemberTest {
+
+    @Test
+    public void givenAValidParams_whenCallsNewMember_thenInstantiateACastMember(){
+        final var expectedName = "Vin Diesel";
+        final var expectedType = CastMemberType.ACTOR;
+
+        final var actualMember = CastMember.newMember(expectedName, expectedType);
+
+        Assertions.assertNotNull(actualMember);
+        Assertions.assertNotNull(actualMember.getId());
+        Assertions.assertEquals(expectedName, actualMember.getName());
+        Assertions.assertEquals(expectedType, actualMember.getType());
+
+        Assertions.assertNotNull(actualMember.getCreatedAt());
+        Assertions.assertNotNull(actualMember.getUpdatedAt());
+        Assertions.assertEquals(actualMember.getCreatedAt(), actualMember.getUpdatedAt());
+    }
+}
