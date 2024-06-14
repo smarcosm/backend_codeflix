a502d2a3a657653239eb7ce7b3a96bc7a3252175
smarcosm <sebastiaomarcos49@gmail.com>
2024-06-14 16:55:04 -0300

    test: :rotating_light: Testes unitarios do caso de uso de listtagem de castmembers

---
+++ application/src/test/java/com/smarcosm/admin_catalogo/application/castmember/retrieve/list/ListCastMemberUseCaseTest.java
@@ -0,0 +1,135 @@
+package com.smarcosm.admin_catalogo.application.castmember.retrieve.list;
+
+import com.smarcosm.admin_catalogo.application.Fixture;
+import com.smarcosm.admin_catalogo.application.UseCaseTest;
+import com.smarcosm.admin_catalogo.application.genre.retrieve.list.DefaultListGenresUseCase;
+import com.smarcosm.admin_catalogo.application.genre.retrieve.list.GenreListOutput;
+import com.smarcosm.admin_catalogo.domain.castmember.CastMember;
+import com.smarcosm.admin_catalogo.domain.castmember.CastMemberGateway;
+import com.smarcosm.admin_catalogo.domain.genre.Genre;
+import com.smarcosm.admin_catalogo.domain.genre.GenreGateway;
+import com.smarcosm.admin_catalogo.domain.pagination.Pagination;
+import com.smarcosm.admin_catalogo.domain.pagination.SearchQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+
+public class ListCastMemberUseCaseTest extends UseCaseTest {
+    @InjectMocks
+    private DefaultListCastMembersUseCase useCase;
+
+    @Mock
+    private CastMemberGateway castMemberGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return List.of(castMemberGateway);
+    }
+
+    @Test
+    public void givenAValidQuery_whenCallsListCastMembers_ShouldReturnAll() {
+        //given
+        final var members = List.of(
+                CastMember.newMember(Fixture.name(), Fixture.CastMember.type()),
+                CastMember.newMember(Fixture.name(), Fixture.CastMember.type()),
+                CastMember.newMember(Fixture.name(), Fixture.CastMember.type()),
+                CastMember.newMember(Fixture.name(), Fixture.CastMember.type())
+        );
+
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "A";
+        final var expectedSort = "createdAt";
+        final var expectedDirect = "asc";
+        final var expectedTotal = 2;
+
+        final var expectedItems =  members.stream().map(CastMemberListOutput::from).toList();
+
+        final var expectedPagination = new Pagination<>(expectedPage, expectedPerPage, expectedTotal, expectedItems);
+
+        when(castMemberGateway.findAll(any())).thenReturn(expectedPagination);
+
+        final var aQuery = new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirect);
+
+        //when
+        final var actualOutput = useCase.execute(aQuery);
+
+        //then
+        Assertions.assertEquals(expectedPage, actualOutput.currentPage());
+        Assertions.assertEquals(expectedPerPage, actualOutput.perPage());
+        Assertions.assertEquals(expectedTotal, actualOutput.total());
+        Assertions.assertEquals(expectedItems, actualOutput.items());
+
+        Mockito.verify(castMemberGateway, times(1)).findAll(eq(aQuery));
+
+    }
+
+    @Test
+    public void givenAValidQuery_whenCallsListCastMemberAndIsEmpty_thenShouldReturn() {
+        //given
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "A";
+        final var expectedSort = "createdAt";
+        final var expectedDirect = "asc";
+        final var expectedTotal = 0;
+
+        final var expectedItems =  List.<CastMemberListOutput>of();
+
+        final var expectedPagination = new Pagination<>(expectedPage, expectedPerPage, expectedTotal, expectedItems);
+
+        when(castMemberGateway.findAll(any())).thenReturn(expectedPagination);
+
+        final var aQuery = new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirect);
+
+        //when
+        final var actualOutput = useCase.execute(aQuery);
+
+        //then
+        Assertions.assertEquals(expectedPage, actualOutput.currentPage());
+        Assertions.assertEquals(expectedPerPage, actualOutput.perPage());
+        Assertions.assertEquals(expectedTotal, actualOutput.total());
+        Assertions.assertEquals(expectedItems, actualOutput.items());
+
+        Mockito.verify(castMemberGateway, times(1)).findAll(eq(aQuery));
+    }
+
+    @Test
+    public void givenAValidQuery_whenCallsListCastMemberAndGatewayThrowsRandomError_ShouldReturnException() {
+        //given
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "A";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+
+        final var expectedErrorMessage = "Gateway error";
+        final var expectedPagination = new Pagination<>(expectedPage, expectedPerPage, expectedTotal, expectedItems);
+
+        when(castMemberGateway.findAll(any())).thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        final var aQuery = new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        //when
+        final var actualException = Assertions.assertThrows(IllegalStateException.class, ()->{
+            useCase.execute(aQuery);
+        });
+
+        //then
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        Mockito.verify(castMemberGateway, times(1)).findAll(eq(aQuery));
+
+
+    }
+}
