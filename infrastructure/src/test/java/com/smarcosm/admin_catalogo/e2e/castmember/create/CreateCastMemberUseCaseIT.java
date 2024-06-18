9d0db2b292f400bc24b636c1e2a6a9b97e9aab56
smarcosm <sebastiaomarcos49@gmail.com>
2024-06-18 17:06:56 -0300

    test: :rotating_light: Testes integrados do caso de uso de criacao de castmember

---
+++ infrastructure/src/test/java/com/smarcosm/admin_catalogo/e2e/castmember/create/CreateCastMemberUseCaseIT.java
@@ -0,0 +1,98 @@
+package com.smarcosm.admin_catalogo.e2e.castmember.create;
+
+import com.smarcosm.admin_catalogo.Fixture;
+import com.smarcosm.admin_catalogo.IntegrationTest;
+import com.smarcosm.admin_catalogo.application.castmember.create.CreateCastMemberCommand;
+import com.smarcosm.admin_catalogo.application.castmember.create.CreateCastMemberUseCase;
+import com.smarcosm.admin_catalogo.domain.castmember.CastMemberGateway;
+import com.smarcosm.admin_catalogo.domain.castmember.CastMemberType;
+import com.smarcosm.admin_catalogo.domain.exception.NotificationException;
+import com.smarcosm.admin_catalogo.infrastructure.castmember.persistence.CastMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@IntegrationTest
+public class CreateCastMemberUseCaseIT {
+    @Autowired
+    private CreateCastMemberUseCase useCase;
+    @Autowired
+    private CastMemberRepository castMemberRepository;
+    @SpyBean
+    private CastMemberGateway castMemberGateway;
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateCastMember_shouldReturnIt() {
+        // given
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type();
+
+        final var aCommand = CreateCastMemberCommand.with(expectedName, expectedType);
+
+        // when
+        final var actualOutput = useCase.execute(aCommand);
+
+        // then
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+        final var actualMember = this.castMemberRepository.findById(actualOutput.id()).get();
+
+        Assertions.assertEquals(expectedName, actualMember.getName());
+        Assertions.assertEquals(expectedType, actualMember.getType());
+        Assertions.assertNotNull(actualMember.getCreatedAt());
+        Assertions.assertNotNull(actualMember.getUpdatedAt());
+        Assertions.assertEquals(actualMember.getCreatedAt(), actualMember.getUpdatedAt());
+
+        verify(castMemberGateway).create(any());
+    }
+
+    @Test
+    public void givenAInvalidName_whenCallsCreateCastMember_shouldThrowsNotificationException() {
+        // given
+        final String expectedName = null;
+        final var expectedType = Fixture.CastMember.type();
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var aCommand = CreateCastMemberCommand.with(expectedName, expectedType);
+
+        // when
+        final var actualException = Assertions.assertThrows(NotificationException.class, () -> {
+            useCase.execute(aCommand);
+        });
+
+        // then
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        verify(castMemberGateway, times(0)).create(any());
+    }
+    @Test
+    public void givenAInvalidType_whenCallsCreateCastMember_shouldThrowsNotificationException() {
+        // given
+        final var expectedName = Fixture.name();
+        final CastMemberType expectedType = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'type' should not be null";
+
+        final var aCommand = CreateCastMemberCommand.with(expectedName, expectedType);
+
+        // when
+        final var actualException = Assertions.assertThrows(NotificationException.class, () -> {
+            useCase.execute(aCommand);
+        });
+
+        // then
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        verify(castMemberGateway, times(0)).create(any());
+    }
+}
