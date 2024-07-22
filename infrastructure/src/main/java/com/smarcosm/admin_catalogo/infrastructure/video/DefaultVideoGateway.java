8cb16d4334e67e99a3ab08d50d014379254378ac
smarcosm <sebastiaomarcos49@gmail.com>
2024-07-22 20:51:54 -0300

    feat: :sparkles: Implementado o metodo create do gateway de video

---
+++ infrastructure/src/main/java/com/smarcosm/admin_catalogo/infrastructure/video/DefaultVideoGateway.java
@@ -0,0 +1,48 @@
+package com.smarcosm.admin_catalogo.infrastructure.video;
+
+import com.smarcosm.admin_catalogo.domain.pagination.Pagination;
+import com.smarcosm.admin_catalogo.domain.video.Video;
+import com.smarcosm.admin_catalogo.domain.video.VideoGateway;
+import com.smarcosm.admin_catalogo.domain.video.VideoID;
+import com.smarcosm.admin_catalogo.domain.video.VideoSearchQuery;
+import com.smarcosm.admin_catalogo.infrastructure.video.persistence.VideoJpaEntity;
+import com.smarcosm.admin_catalogo.infrastructure.video.persistence.VideoRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class DefaultVideoGateway implements VideoGateway {
+    private final VideoRepository videoRepository;
+
+    public DefaultVideoGateway(final VideoRepository videoRepository) {
+        this.videoRepository = Objects.requireNonNull(videoRepository);
+    }
+
+    @Override
+    @Transactional
+    public Video create(final Video aVideo) {
+        return this.videoRepository.save(
+                VideoJpaEntity.from(aVideo)).toAggregate();
+    }
+
+    @Override
+    public void deleteById(VideoID anId) {
+
+    }
+
+    @Override
+    public Optional<Video> findById(VideoID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Pagination<Video> findAll(VideoSearchQuery aQuery) {
+        return null;
+    }
+
+    @Override
+    public Video update(Video aVideo) {
+        return null;
+    }
+}
--- infrastructure/src/main/resources/db/migration/V3__Create_CastMember_Aggregate.sql
+++ infrastructure/src/main/resources/db/migration/V3__Create_CastMember_Aggregate.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE cast_members (
-    id CHAR(32) NOT NULL PRIMARY KEY,
+id CHAR(32) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     type VARCHAR(32) NOT NULL,
     created_at DATETIME(6) NOT NULL,
